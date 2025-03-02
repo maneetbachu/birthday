e615851c7e31c2fdb4fea9967bbdad652d674d19
maneetbachu <maneetbachu2005@gmail.com>
2025-03-02 23:33:32 +0530

    Update app1.js

--- app1.js
+++ app1.js
@@ -1,21 +1,20 @@
 document.addEventListener("DOMContentLoaded", function () {
     function createFlower() {
         let flower = document.createElement("div");
-        flower.innerHTML = '<img src="heart.png" style="width:40px; height:40px;">'; // Flower Emoji
+        flower.innerHTML = '<img src="heart.png" style="width:40px; height:40px;">'; 
         flower.classList.add("flower");
 
         // Random position
         flower.style.left = Math.random() * window.innerWidth + "px";
-        flower.style.animationDuration = (Math.random() * 7 + 5) + "s"; // Random fall speed
-        flower.style.fontSize = (Math.random() * 10 + 50) + "px"; // Random size
+        flower.style.animationDuration = (Math.random() * 7 + 5) + "s"; 
+        flower.style.fontSize = (Math.random() * 10 + 50) + "px"; 
 
         document.body.appendChild(flower);
 
         setTimeout(() => {
             flower.remove();
-        }, 10000); // Remove flower after animation
+        }, 10000); 
     }
 
-    // Generate flowers every 200ms
     setInterval(createFlower, 50);
 });
